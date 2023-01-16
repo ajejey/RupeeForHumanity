e04af746af9ea4bfbe483618171bf349972f783b
Ajey <ajejey@gmail.com>
2023-01-16 21:30:16 +0530

    create node app

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static(__dirname));
+
+app.use(express.static("public"));
+
+// app.get('/', function (req, res) {
+//     res.sendFile(__dirname + '/index.html')
+// })
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000")
+})
