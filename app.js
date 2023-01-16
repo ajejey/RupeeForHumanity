19c7e838a6349d971ec29e2a5dccf9e8ee825929
Ajey <ajejey@gmail.com>
2023-01-16 21:39:22 +0530

    port changed

--- app.js
+++ app.js
@@ -9,6 +9,6 @@ app.use(express.static("public"));
 //     res.sendFile(__dirname + '/index.html')
 // })
 
-app.listen(3000, () => {
-    console.log("Server started on port 3000")
-})
+app.listen(process.env.PORT || 3000, function () {
+    console.log("server started on port 3000");
+});
